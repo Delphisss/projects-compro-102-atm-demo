b2bf2fb073a77410a88b2785982b6eb6f5a6d779
Delphisss <Kong_led_@hotmail.com>
2023-02-23 01:36:16 +0700

    โอเคไปส่วนใหญ่แล้ว

--- atm-demo.cpp
+++ atm-demo.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <conio.h> // for getch() function
 #include <iomanip>
+#include <windows.h> //sleep
 
 using namespace std;
-
+int id, password; //login
+char ch; //login && ATM
+int choice = 1; //ATM
 float balance = 2000; //ลบออก
 
-void highlight(int index, bool selected) { 
+//higlight atm menu
+void highlight_ATM_MENU(int index, bool selected) { 
     int l = 0 ;
     
     if (selected) {
@@ -39,12 +43,46 @@ void highlight(int index, bool selected) {
     cout << "\033[0m"  << setw(l)<<" | \n"; // window
 }
 
-int main() {
-    //login
-    int id, password;
-    char ch;
+//highlight_Login_failed
+void highlight_Login_failed(int index, bool selected) { 
+    int l = 0 ;
+    
+    if (selected) {
+        cout << "\033[1;7m"; // set background and foreground color to white and black
+    }
+    cout << index << ". ";
 
+    // print the option label
+    switch (index) {
+        case 1:
+            l = 29 ;
+            cout << "Please try again.";
+            break;
+        default:
+            break;
+    }
+
+    cout << "\033[0m"  << setw(l)<<" | \n"; // window
+}
+
+//Login failed
+int Login_failed (){
+    system("cls");
+    cout << "+-------------------------------------+" ;
+    cout << "\nLogin failed. Incorrect ID or password. \n" ;
+    cout << "+-------------------------------------+\n" ;
+    cout << setw(28) <<"Please try again." ;
+    cout << "\n+-------------------------------------+\n" ;
+}
+
+
+
+//Login ATM
+int login(){
+    //int id, password;
+    //char ch;
     // Display login window frame
+    system("cls");
     cout << "+-------------------------------------+\n" ;
     cout << "|               Login ATM             |\n" ;
     cout << "+-------------------------------------+\n" ;
@@ -67,81 +105,91 @@ int main() {
             password = password * 10 + (ch - '0');
             cout << "*";
         }
-    }    
-    
+    } 
+}
+
+
+int main(){
+    //login
+    login();
     if (id-650610000 == password ) {
-        cout << "Login successful! \n" ;
-                        // ATM
-                        int choice = 1;
-                        char ch;
-                        do {
-                            system("cls"); // clear the console
-                            cout << "+-------------------------------------+\n";
-                            cout << "|              ATM Menu               |\n"; 
-                            cout << "+-------------------------------------+\n";
-                            // display the menu options
-                            for (int i = 1; i <= 4; i++) {
-                                cout << "| ";
-                                highlight(i, i == choice);
-                                //cout << " |";
-                            }
-                            cout << "|                                     |\n";
-                            cout << "+-------------------------------------+\n";
+        system("cls");
+        cout << "+-------------------------------------+\n";
+        cout << "|          Login successful!          |\n";
+        cout << "+-------------------------------------+\n";
+        Sleep(2000); // Wait for 1000 milliseconds
+        //ATM
+        do {
+            system("cls"); // clear the console
+            cout << "+-------------------------------------+\n";
+            cout << "|              ATM Menu               |\n"; 
+            cout << "+-------------------------------------+\n";
+            // display the menu options
+            for (int i = 1; i <= 4; i++) {
+            cout << "| ";
+            highlight_ATM_MENU(i, i == choice);
+            }
+            
+            cout << "|                                     |\n";
+            cout << "+-------------------------------------+\n";
 
-                            ch = getch(); // wait for a key press
+            ch = getch(); // wait for a key press
 
-                            // update the choice variable based on the arrow key input
-                            if (ch == 72 && choice > 1) { // up arrow key
-                                choice--;
-                            }
-                            else if (ch == 80 && choice < 4) { // down arrow key
-                                choice++;
-                            }
-                        } while (ch != 13); // enter key
+            // update the choice variable based on the arrow key input
+            if (ch == 72 && choice > 1) { // up arrow key
+                choice--;
+            }
+            else if (ch == 80 && choice < 5) { // down arrow key
+                choice++;
+            }
+            } while (ch != 13); // enter key
 
-                        // display the selected option
-                        switch (choice) {
-                            case 1:
-                                // Deposit
-                                double depositAmount;
-                                cout << "Enter the amount to deposit: ";
-                                cin >> depositAmount;
-                                if (depositAmount > 0) {
-                                    balance += depositAmount;
-                                    cout << "Deposit successful! Your new balance is: $" << fixed << setprecision(2) << balance << "\n";
-                                } else {
+                // display the selected option
+                switch (choice) {
+                    case 1:
+                        // Deposit
+                        double depositAmount;
+                        cout << "Enter the amount to deposit: ";
+                        cin >> depositAmount;
+                                
+                        if (depositAmount > 0) {
+                            balance += depositAmount;
+                            cout << "Deposit successful! Your new balance is: $" << fixed << setprecision(2) << balance << "\n";
+                         } else {
                                     cout << "Invalid deposit amount.\n";
                                 }
                                 break;
-                            case 2:
-                                // Withdraw
-                                double withdrawAmount;
-                                cout << "Enter the amount to withdraw: ";
-                                cin >> withdrawAmount;
-                                if (withdrawAmount <= balance && withdrawAmount > 0) {
-                                    balance -= withdrawAmount;
-                                    cout << "Withdrawal successful! Your new balance is: $" << fixed << setprecision(2) << balance << "\n";
-                                } else {
+                    
+                    case 2:
+                        // Withdraw
+                        double withdrawAmount;
+                        cout << "Enter the amount to withdraw: ";
+                        cin >> withdrawAmount;
+                        if (withdrawAmount <= balance && withdrawAmount > 0) {
+                                balance -= withdrawAmount;
+                                cout << "Withdrawal successful! Your new balance is: $" << fixed << setprecision(2) << balance << "\n";
+                        } else {
                                     cout << "Invalid withdrawal amount.\n";
                                 }
                                 break;
-                            case 3:
-                                // Balance Inquiry
-                                cout << "Your current balance is: $" << fixed << setprecision(2) << balance << "\n";
+                    
+                    case 3:
+                        // Balance Inquiry
+                        cout << "Your current balance is: $" << fixed << setprecision(2) << balance << "\n";
                                 break;
-                            case 4:
-                                // Exit
-                                cout << "Thank you for using this ATM. Goodbye!\n";
-                                exit(0);  // terminate the program
-                                break;
+                    case 4:
+                        // Exit
+                        cout << "Thank you for using this ATM. Goodbye!\n";
+                        main();// Return to login page 
+                        break;
                         }
 
-                        return 0;
-    
+                        return 0; 
     } else {
-        cout << "\n\n+-------------------------------------+" ;
-        cout << "\nLogin failed. Incorrect ID or password. \n" ;
-        cout << "+-------------------------------------+\n" ;
+        Login_failed();
+        Sleep(2000); // Wait for 1000 milliseconds
+        main();
+                 
     }
     
 }
